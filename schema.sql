60e0037d9f9d6217f112b655b09398d93253dd22
James <James.Hacklander@gmail.com>
2021-05-25 15:15:45 -0700

    Added seeds file. Fixed syntax in Schema.

--- schema.sql
+++ schema.sql
@@ -1,22 +1,22 @@
 -- Scheema
--- DROP DATABASE IF EXISTS employees_db
-CREATE DATABASE employees_db
-USE employees_db
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+USE employees_db;
 
 CREATE TABLE employee (
-    id INT NOT NULL
+    id INT NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50),
     role_id INT, -- employee role
     manager_id INT, -- reference to this employee's manager, may be NULL
     PRIMARY KEY(id)
-)
+);
 
 CREATE TABLE department (
     id INT NOT NULL,
-    name VARCHAR(30) -- name of department for this employee
+    name VARCHAR(30), -- name of department for this employee
     PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE role (
     id INT NOT NULL,
@@ -24,4 +24,8 @@ CREATE TABLE role (
     salary DEC,
     department_id INT, -- reference to department the role belongs to
     PRIMARY KEY (id)
-)
+);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
---
+++ seeds.sql
@@ -0,0 +1,85 @@
+USE employeesDB;
+
+-- DEPARTMENT SEEDS
+INSERT INTO department (id, name)
+VALUES (1, 'Development');
+
+INSERT INTO department (id, name)
+VALUES (2, 'Marketing');
+
+INSERT INTO department (id, name)
+VALUES (3, 'Human Resources');
+
+INSERT INTO department (id, name)
+VALUES (4, 'Legal');
+
+INSERT INTO department (id, name)
+VALUES (5, 'Finance');
+
+-- ROLE SEEDS
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, 'Front end Developer', 100000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (2, 'Back end Developer', 100000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (3, 'Project Manager', 130000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (4, 'Marketing Analyst', 80000, 2);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (5, 'Recruiter', 75000, 3);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (6, 'Staff Coordinator', 60000, 3);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (7, 'Lawyer', 110000, 4);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (8, 'Accountant', 55000, 5);
+
+-- EMPLOYEE SEEDS
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, 'Remy', 'Martin', 1, 3);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (2, 'Vera', 'Dillard', 1, 3);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (3, 'Annika', 'Ramirez', 2, 3);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (4, 'Jermaine', 'Dotson', 2, 3);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (5, 'Tyla', 'Talbot', 3, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (6, 'Amit', 'Lancaster', 4, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (7, 'Avery', 'Michael', 4, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (8, 'Hector', 'Moyer', 5, 10);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (9, 'Douglas', 'Byrne', 5, 10);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (10, 'Kaitlan', 'Haines', 6, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (11, 'Andy', 'Rivera', 7, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (12, 'Skylar', 'Rasmussen', 7, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (13, 'Cairon', 'Chung', 8, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (14, 'Derry', 'Stout', 8, null);
